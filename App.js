90285c0fce637d2f6f9e2a7f6e03f298875fb900
Jitgitbit <rowowolf@gmail.com>
2020-03-14 16:01:11 +0100

    Header made

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -9,7 +10,7 @@ export default function App() {
   ])
   return (
     <View style={styles.container}>
-      {/* header */}
+      <Header/>
       <View style={styles.content}>
         {/* todo form */}
         <View style={styles.list}>
@@ -28,7 +29,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
