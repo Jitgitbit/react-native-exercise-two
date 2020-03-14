1e30aa9b564b64807992950852c48c425544abc6
Jitgitbit <rowowolf@gmail.com>
2020-03-14 15:48:17 +0100

    preparation done

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text:'buy coffee', key:'1'},
+    {text:'pyramid exercise', key:'2'},
+    {text:'breathing exercise', key:'3'},
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* header */}
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+          data={todos}
+          renderItem={({item}) => (
+          <Text>{item.text}</Text>
+          )}/>
+        </View>
+      </View>
     </View>
   );
 }
